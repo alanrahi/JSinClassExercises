687a19c63cf2c5e79a4498d91089a0431ac198cc
Alan Rahi <alan@webfordoctors.com>
2015-01-13 14:20:43 -0800

    maze game is pretty much done

--- JS5thDay/adventure_game.js
+++ JS5thDay/adventure_game.js
@@ -3,9 +3,6 @@ var map = require(process.argv[2]);
 var gameMaker = require('jsi-gamelib').map;
 
 var prompt = require('prompt');
-
-console.log(gameMaker);
-
 //puts player into the entrance room
 var currentRoom;
 	
@@ -14,21 +11,15 @@ var currentRoom;
 		if (room.entrance) {currentRoom = room;}
 		});
 	
-		
-
-
-	
-
-
 prompt.start();
 
-var roomKeys = { "a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8, "i":9, "j":10, "k":11}
+//var roomKeys = { "a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8, "i":9, "j":10, "k":11}
 
 var affordances = [];
 
 
-function WhereCanYouGo(room) {
- 
+function WhereCanYouGo(room) 
+{
 	
 	affordances = [];
 	
@@ -41,93 +32,69 @@ function WhereCanYouGo(room) {
 	
 }
 
+function Moves () 
 
-var WhereYouCanGo = WhereCanYouGo(player.inRoom);
-
-var property = {
-	direction: 'input',
-	message: "You are standing in a room. There \
-are " + WhereYouCanGo.length + " Doors. You can go " + WhereYouCanGo + " Please choose a direction."
-};
-
-prompt.get(property, function(err, input) 
 {
-		var move = input;
-		console.log(input);
+		var WhereYouCanGo = WhereCanYouGo(currentRoom);
 
-	if (input === "north")
-		{ maps.rooms.forEach(function(room) 
-			{
-				if (room.name === currentRoom.north) {currentRoom = room;}
-			});
+		var property = {
+			direction: 'input',
+			message: "You are standing in a room. There are " + WhereYouCanGo.length + " Doors. You can go " + WhereYouCanGo + " Please choose a direction."
 		};
 
-	if (input === "south")
-		{ maps.rooms.forEach(function(room)	
-			{
-				if (room.name === currentRoom.north) {currentRoom = room;}
-			});
-		};
+		prompt.get(property, function(err, input) 
+		{
+				//var move = input;
+				//console.log(input);
 
-	if (input === "east") 
-		{ maps.rooms.forEach(function(room) 
-			{
-				if (room.name === currentRoom.east) {currentRoom = room;}
-			});
-		}
+			if (input.question === "north")
+				{ map.rooms.every(function(room) 
+					{
+						if (room.name === currentRoom.north) {currentRoom = room; return false;}
+						else { return true;}
+					});	
+				};
 
-	if (input === "west") 
-		{ maps.rooms.forEach(function(room) 
-			{
-				if (room.name === currentRoom.west) {currentRoom = room;}
-			});
-		}	
-});
+			if (input.question === "south")
+				{ map.rooms.every(function(room)	
+					{
+						if (room.name === currentRoom.south) {currentRoom = room; return false;}
+						else { return true;}
+					});
+						
+				};
 
-var WhereYouCanGo = WhereCanYouGo(player.inRoom);
+			if (input.question === "east") 
+				{ map.rooms.every(function(room) 
+					{
+						if (room.name === currentRoom.east) {currentRoom = room; return false;}
+						else { return true;}
+					});
+						
+				}
 
-var property = {
-	direction: 'input',
-	message: "You are standing in a room. There \
-are " + WhereYouCanGo.length + " Doors. You can go " + WhereYouCanGo + " Please choose a direction."
-};
+			if (input.question === "west") 
+				{ map.rooms.every(function(room) 
+					{
+						if (room.name === currentRoom.west) {currentRoom = room; return false;}
+						else { return true;}
+					});
+						
+				}
 
-prompt.get(property, function(err, input) 
-{
-		var move = input;
-		console.log(input);
+			
+				console.log(currentRoom);	
+				Moves();	
 
-	if (input === "north")
-		{ maps.rooms.forEach(function(room) 
-			{
-				if (room.name === currentRoom.north) {currentRoom = room;}
-			});
-		};
+		});
+}
+	
 
-	if (input === "south")
-		{ maps.rooms.forEach(function(room)	
-			{
-				if (room.name === currentRoom.north) {currentRoom = room;}
-			});
-		};
+	Moves();
 
-	if (input === "east") 
-		{ maps.rooms.forEach(function(room) 
-			{
-				if (room.name === currentRoom.east) {currentRoom = room;}
-			});
-		}
+    	
 
-	if (input === "west") 
-		{ maps.rooms.forEach(function(room) 
-			{
-				if (room.name === currentRoom.west) {currentRoom = room;}
-			});
-		}	
-});
-
-
-
+			
 
 
 
